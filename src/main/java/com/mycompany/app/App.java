8cad5e028e8dc86fa213687b6848fbbbb5427fda
xiaoqi <yixiaoqi2010@163.com>
2020-04-13 19:54:19 +0800

    test jenkins

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,7 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+        System.out.println(111111L);
     }
 
     private final String getMessage() {
